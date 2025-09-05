1f89a1c4cd55c0edd26e344e2e0528a391d1e30e
quiettimejsg <188880511+quiettimejsg@users.noreply.github.com>
2025-09-06 03:40:10 +0800

    docs(china-special-version): update compliance statement text
    Update the modal content to better reflect compliance requirements and improve clarity of the message for mainland China users. The changes include more precise wording about data handling and version availability.

--- components/china-special-version.js
+++ components/china-special-version.js
@@ -29,25 +29,20 @@ class ChinaSpecialVersionModal {
     overlay.innerHTML = `
       <div class="modal-content">
         <div class="modal-header">
-          <h2 class="modal-title">特别版本提示/Special Version</h2>
+          <h2 class="modal-title">合规声明 / Compliance Statement</h2>
         </div>
         <div class="modal-body">
-          <p>您当前访问的是自介网站的中国大陆特别版本</p>
-          <p>You are currently accessing the special version of the self-introduction website for mainland China.</p>
-          <p>此版本不会收集、传输或分析您的任何数据，请放心使用。</p>
-          <p>This version will not collect, transmit, or analyze any of your data. Please use it with confidence.</p>
-          <p>其他语言（英语、日语、繁体中文等）暂不适用。</p>
-          <p>Other languages (English, Japanese, Traditional Chinese, etc.) are not available for this version currently.</p>
-          <p>若您地理位置未在中国大陆区域，并希望访问国际版多语言网站，请手动访问国际版网站：</p>
-          <p>If you are not located in mainland China and wish to access the international multi-language website, please manually visit the international version:</p>
-          <p style="text-align: center;">https://chronie-shizutoki.github.io/self-info-ja/</p>
-          <p>若您地理位置位于中国大陆区域，您只能使用此特别版本。</p>
-          <p>If you are located in mainland China, you can only use this special version.</p>
+          <p>您当前访问的是自介网站为中国大陆用户提供的特别版本，该版本遵循相关法律法规要求。</p>
+          <p>You are currently accessing the special version of our website provided for users in mainland China in compliance with the relevant laws and regulations.</p>
+          <p>根据要求，此版本不会收集、传输或分析您的任何数据，请放心使用。</p>
+          <p>As required, this version does not collect, transmit, or analyze any of your data. Please use it with confidence.</p>
+          <p>如果您的地理位置位于中国大陆，请使用此版本。国际版网站（提供多语言服务）暂不适用于中国大陆用户。</p>
+          <p>If you are located in mainland China, please use this version. The international website (which offers multi-language services) is currently not available for users within mainland China.</p>
           <p>若您有任何问题或建议，请联系我们。</p>
           <p>If you have any questions or suggestions, please contact us.</p>
         </div>
         <div class="modal-footer">
-          <button class="modal-button" id="close-modal-btn">我知道了/Understood</button>
+          <button class="modal-button" id="close-modal-btn">我知道了 / Understood</button>
         </div>
       </div>
     `;
